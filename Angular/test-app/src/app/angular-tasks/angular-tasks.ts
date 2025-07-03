4e2486c57bd56f87b7d035aacc28d27c0aba6de3
Valeriia Iavorskaia <valeriya.yavorskaya@gmail.com>
2025-07-03 16:27:11 +0200

    feat: beautification

--- Angular/test-app/src/app/angular-tasks/angular-tasks.ts
+++ Angular/test-app/src/app/angular-tasks/angular-tasks.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-angular-tasks',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './angular-tasks.html',
   styleUrl: './angular-tasks.scss',
   standalone: true,
